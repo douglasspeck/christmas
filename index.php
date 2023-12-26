1b5b515745d1559cc63a8ab0cfff2eefb9a9e305
Douglas Speck <speck.empresarial@gmail.com>
2023-12-25 22:41:22 -0300

    Finished tenth message

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
             <section id="tree-branches">
                 <section id="bottom-branches">
                     <div class="branch"><a href=""><span class="branch"></span></a></div>
-                    <div class="branch"><a href=""><span class="branch"></span></a></div>
+                    <div class="branch"><a href="letters.php?q=malu&p=a"><span class="branch">Malu</span></a></div>
                     <div class="branch"><a href="letters.php?q=alana&p=a"><span class="branch">Alana</span></a></div>
                     <div class="branch"><a href="letters.php?q=lena&p=a"><span class="branch">Lena</span></a></div>
                     <div class="branch"><a href="letters.php?q=julia&p=a"><span class="branch">Júlia</span></a></div>
@@ -54,7 +54,7 @@
                 </section>
                 <section id="middle-branches">
                     <div class="branch"><a href=""><span class="branch"></span></a></div>
-                    <div class="branch"><a href=""><span class="branch"></span></a></div>
+                    <div class="branch"><a href="letters.php?q=ary&p=o"><span class="branch">Ary</span></a></div>
                     <div class="branch"><a href="letters.php?q=miau&p=o"><span class="branch">Miau</span></a></div>
                     <div class="branch"><a href="letters.php?q=pedro&p=o"><span class="branch">Pedro</span></a></div>
                     <div class="branch"><a href="letters.php?q=fur&p=o"><span class="branch">Fur</span></a></div>
