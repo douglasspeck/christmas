fbc40cc094660a07e8d81e5055a7450b6ae990bc
Douglas Speck <speck.empresarial@gmail.com>
2023-12-25 22:38:49 -0300

    Finished tenth message

--- letters.php
+++ letters.php
@@ -25,11 +25,11 @@
         "pedro" => new Person("Pedro", "saboneteliquido"),
         "miau" => new Person("Miau", "rioclaro"),
         "alana" => new Person("Alana", "loirinha"),
+        "ary" => new Person("Ary", "carioca"),
+        "malu" => new Person("Malu", "paulista"),
         "mae" => new Person("Edna", ""),
         "denis" => new Person("Denis", "meiosangue"),
         "diogo" => new Person("Diogo", "minecraft"),
-        "ary" => new Person("Ary", "carioca"),
-        "malu" => new Person("Malu", "paulista"),
     ];
 
     if (!isset($_GET["q"])) { header("Location: /"); }
